2e2314ed82be839e62e1857b37f15f2a849c9fd5
jovan.manojlovic <jovan.manojlovic@fivetran.com>
2024-01-11 16:24:29 +0100

    Format code

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,38 +1,82 @@
 local cmp = require "cmp"
 
 local plugins = {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "rust-analyzer",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
     {
-        "nvimtools/none-ls.nvim",
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "rust-analyzer",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
         config = function()
-            local nullLs = require("null-ls")
-            nullLs.setup({
-                sources = {
-                    nullLs.builtins.formatting.stylua,
-                },
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "simrat39/rust-tools.nvim",
+        ft = "rust",
+        dependencies = "neovim/nvim-lspconfig",
+        opts = function()
+            return require "custom.configs.rust-tools"
+        end,
+        config = function(_, opts)
+            require('rust-tools').setup(opts)
+        end
+    },
+    {
+        "mfussenegger/nvim-dap",
+        init = function()
+            require("core.utils").load_mappings("dap")
+        end
+    },
+    {
+        'saecki/crates.nvim',
+        ft = { "toml" },
+        config = function(_, opts)
+            local crates = require('crates')
+            crates.setup(opts)
+            require('cmp').setup.buffer({
+                sources = { { name = "crates" } }
             })
-
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            crates.show()
+            require("core.utils").load_mappings("crates")
+        end,
+    },
+    {
+        "rust-lang/rust.vim",
+        ft = "rust",
+        init = function()
+            vim.g.rustfmt_autosave = 1
+        end
+    },
+    {
+        "theHamsta/nvim-dap-virtual-text",
+        lazy = false,
+        config = function(_, opts)
+            require("nvim-dap-virtual-text").setup()
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function()
+            local M = require "plugins.configs.cmp"
+            M.completion.completeopt = "menu,menuone,noselect"
+            M.mapping["<CR>"] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = false,
+            }
+            table.insert(M.sources, { name = "crates" })
+            return M
         end,
     },
     {
         'kristijanhusak/vim-dadbod-ui',
         dependencies = {
-            { 'tpope/vim-dadbod', lazy = true },
+            { 'tpope/vim-dadbod',                     lazy = true },
             { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
         },
         cmd = {
@@ -42,8 +86,8 @@ local plugins = {
             'DBUIFindBuffer',
         },
         init = function()
-        -- Your DBUI configuration
-        vim.g.db_ui_use_nerd_fonts = 1
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
         end,
     }
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,1 +1,3 @@
-vim.api.nvim_set_keymap('n', '<leader>Cr', ':belowright split term://cargo run<CR>:resize 10<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>Cr', ':belowright split term://cargo run<CR>:resize 10<CR>',
+    { noremap = true, silent = true })
+vim.cmd("set colorcolumn=120")
